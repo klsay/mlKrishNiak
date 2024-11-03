32b365359d3b1be155ae3e1c1a07db28c5ddcd31
klsay <kinlee@hotmail.com>
2024-11-03 20:45:08 +0800

    Data Ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,54 @@
+import os
+import sys
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent))
+from exception import CustomException
+from logger import logging
+
+@dataclass
+class DataIngestionConfig:
+    logging.info("DataIngestionConfig")
+    train_data_path: str=os.path.join('artifacts','train.csv')
+    test_data_path: str=os.path.join('artifacts','test.csv')
+    raw_data_path: str=os.path.join('artifacts','raw.csv')
+    
+class DataIngestion:
+    def __init__(self):
+        logging.info("DataIngestion Constructor")
+        self.ingestion_config=DataIngestionConfig()
+        
+    def initiate_data_ingestion(self):
+        logging.info("Data injestion method or component")
+        try:
+            df=pd.read_csv("notebook/data/stud.csv")
+            logging.info("Read the dataset as dataframe")
+            
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+            
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+            
+            logging.info("Train test split initiated")
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+            
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+            
+            logging.info("Injestion of data is completed")
+            
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )            
+        except Exception as e: 
+            raise CustomException(e, sys)
+        
+if __name__=="__main__":
+    logging.info("main here")
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
+    
--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,7 @@
 import sys
+
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent))
 from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
@@ -16,10 +19,3 @@ class CustomException(Exception):
         
     def __str__(self):
         return self.error_message
-
-if __name__ == "__main__":    
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Divide by Zero")
-        raise CustomException(e, sys)
